50e855eeaab8623f17f039524ecace347aafa44f
Willians Martins <professorwillians@gmail.com>
2023-05-27 12:46:09 -0700

    start

---
+++ script.js
@@ -0,0 +1,10 @@
+var app = angular.module('app', []);
+
+app.controller('Controller1', function($scope, $http){
+    // $scope.grupos = [];
+
+    // $http.get('modelo.json').then(function(response){
+    //     $scope.grupos = response.data.grupos;
+    // });
+});
+
