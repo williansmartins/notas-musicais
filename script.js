693318abe87a500fabe5b7066cff0d1c54c80d7d
Willians Martins <professorwillians@gmail.com>
2023-06-08 22:54:08 -0700

    teste no celular

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 var app = angular.module('app', []);
 
 app.controller('Controller1', function($scope, $http){
-    // $scope.grupos = [];
-
-    // $http.get('modelo.json').then(function(response){
-    //     $scope.grupos = response.data.grupos;
-    // });
+    //alert(123);
 });
 
