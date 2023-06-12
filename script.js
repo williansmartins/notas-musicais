31394cec92115413766f1606be9da2a190e72cb4
Willians Martins <professorwillians@gmail.com>
2023-06-12 11:07:21 -0700

    add timer e escondendo botoes

--- script.js
+++ script.js
@@ -55,13 +55,13 @@ app.controller('Controller1', function($scope, $timeout){
             $scope.acertou = false;
         }
 
-        $timeout(limparResultado, 2000);
-        novaRodada();
+        $timeout(limparResultado, 3000);
+        
     }
 
     function limparResultado(){
         $scope.acertou = null;
-        
+        novaRodada();
     }
 
     function pad(val) {
@@ -73,6 +73,25 @@ app.controller('Controller1', function($scope, $timeout){
         }
     }
 
+    function googleAnalytics(){
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        
+        ga('create', 'UA-3749716-2', 'auto');
+        ga('send', 'pageview');
+        
+        ga('send', 'event', 'entrou_no_jogo', $("body").attr('data-page'), 'entrou mesmo');
+    
+        $("a").click(function(e){
+            // e.preventDefault();
+            ga('send', 'event', 'click', $(this).attr('href') , '');
+            // alert($(this).attr('href'));
+        });
+    }
+
+    googleAnalytics();
     novaRodada();
 });
 
